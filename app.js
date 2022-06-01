04966c90b0f9c69343eb1dfb117fb92d285dea26
Your Name <onlyonezukee@gmail.com>
2022-05-31 23:31:11 -0700

    header

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var typed = new Typed("#typing", {
+        strings: ["Mechanical Engineer", "Student", "Game Developer", "Creator"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
