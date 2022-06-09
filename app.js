6816bfd578af2f418bb4e406ea46bbedfff15a89
Zuriel Joven <onlyonezukee@gmail.com>
2022-06-08 20:41:38 -0700

    Updates

--- app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var typed = new Typed("#typing", {
-        strings: ["Mechanical Engineer", "Student", "Game Developer", "Creator"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-});
